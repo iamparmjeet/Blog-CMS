b29215203b10f7663e97151faf564d5b56cc0791
Parmjeet Mishra <iamparmjeetmishra@gmail.com>
2026-09-22 16:25:22 +0530

    feat(media): add preview variant columns

---
+++ src/db/drizzle/0004_fuzzy_flatman.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `media` ADD `preview_key` text;--> statement-breakpoint
+ALTER TABLE `media` ADD `preview_url` text;--> statement-breakpoint
+ALTER TABLE `media` ADD `preview_type` text;--> statement-breakpoint
+ALTER TABLE `media` ADD `preview_size` text;
